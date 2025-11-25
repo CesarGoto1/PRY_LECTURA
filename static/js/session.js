e00da5037adae4f3c615d1e01492ed8e919bfdb3
David11tene <drteneguznay@espe.edu.ec>
2025-11-25 15:22:48 -0500

    Reorganizacion

---
+++ static/js/session.js
@@ -0,0 +1,24 @@
+
+function cerrarSesion(){
+    localStorage.removeItem("usuario");
+    window.location.href = "/templates/login.html";
+}
+
+function protegerRuta(rolRequerido){
+    const usuario = JSON.parse(localStorage.getItem("usuario"));
+
+    if(!usuario){
+        window.location.href = "/templates/login.html";
+        return;
+    }
+
+    if(rolRequerido === "admin" && usuario.rol !== "admin"){
+        window.location.href = "/templates/usuario/index.html";
+        return;
+    }
+
+    if(rolRequerido === "usuario" && usuario.rol !== "usuario"){
+        window.location.href = "/templates/admin/index.html";
+        return;
+    }
+}
