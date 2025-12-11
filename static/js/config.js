b24c1b4dbc8ef574edb57b826f6639f762f34670
CesarGoto1 <cagonzalez12@espe.edu.ec>
2025-12-11 14:51:01 -0500

    Prueba2

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // En este archivo, reemplaza la URL por la que te proporcionó Railway para tu backend.
-const API_BASE_URL = "https://web-production-9255.up.railway.app/";
+const API_BASE_URL = "https://web-production-9255.up.railway.app";
