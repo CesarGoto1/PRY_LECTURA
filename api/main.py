a8f97f3b61ef2be68e406a3f25a03419c22f62a9
CesarGoto1 <cagonzalez12@espe.edu.ec>
2025-12-11 15:06:07 -0500

    Prueba4

--- api/main.py
+++ api/main.py
@@ -16,18 +16,11 @@ log = logging.getLogger("uvicorn.error")
 
 app = FastAPI()
 
-# --- CONFIGURACIÓN CORS ---
-# Orígenes permitidos para peticiones
-origins = [
-    "https://pry-lectura.vercel.app",  # URL de la aplicación en Vercel
-    "http://localhost",
-    "http://localhost:5500",        # Live Server de VS Code
-    "http://127.0.0.1:5500",        # Live Server de VS Code
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
